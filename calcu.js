bf7f2af8b73bfef56d73488451657c94b173a8f1
Your Name <you@example.com>
2024-10-19 12:48:31 +0530

    calcu

---
+++ calcu.js
@@ -0,0 +1,32 @@
+const displayContent=(content)=>{
+
+    output.value+=content
+}
+
+
+const showoutput=()=>{
+
+
+
+
+
+try{
+  output.value=eval(output.value)
+}
+catch{
+  output.value=null
+
+output.placeholder="invalid syntax"
+
+}
+}
+clearAll=()=>{
+  output.value=null;
+  output.placeholder=0
+
+}
+remove=()=>{
+  output.value=output.value.slice(0,-1)
+
+
+}
